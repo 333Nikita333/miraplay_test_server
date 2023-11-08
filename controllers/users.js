fea662217a28a17b53538d704c5df56254307fd4
333Nikita333 <nikitosfox333@gmail.com>
2023-11-08 21:10:10 +0200

    added current user controller and route

--- controllers/users.js
+++ controllers/users.js
@@ -53,7 +53,7 @@ const login = async (req, res) => {
     id: user._id,
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-  
+
   await User.findByIdAndUpdate(user._id, { token });
 
   res.json({
@@ -64,7 +64,14 @@ const login = async (req, res) => {
   });
 };
 
+const getCurrent = async (req, res) => {
+  const { email } = req.user;
+
+  res.json({ email });
+};
+
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  getCurrent: ctrlWrapper(getCurrent),
 };
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,13 @@
 const express = require("express");
 
 const ctrl = require("../controllers/users");
-const { validateBody } = require("../middlewares");
+const { validateBody, authenticate } = require("../middlewares");
 const { schemas } = require("../schemas/user");
 
 const router = express.Router();
 
 router.post("/register", validateBody(schemas.authSchema), ctrl.register);
 router.post("/login", validateBody(schemas.authSchema), ctrl.login);
+router.get("/current", authenticate, ctrl.getCurrent);
 
 module.exports = router;
